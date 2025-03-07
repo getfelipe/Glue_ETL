9414535b3b9cd718963ac5d4a3459782a9fc848f
getFelipe <felipesbsmachado@outlook.com>
2025-03-06 22:14:38 -0300

    updt lamda: parameterized

--- lambda.py
+++ lambda.py
@@ -3,16 +3,22 @@ import boto3
 
 
 def lambda_handler(event, context):
+
+    s3_record = event['Records'][0]['s3']
+    path_file = s3_record['object']['key']
+
+    folder_table = path_file.split('/')[2]
+
+
     # Inicializa o cliente do AWS Glue
     glue = boto3.client('glue')
 
     try:
         response = glue.start_workflow_run(
-            Name=f"workflow-registers",
+            Name=f"workflow-{folder_table}",
 
         )
         print("Sucesso!!!")
     except Exception as e:
         print(f"Erro ao iniciar o workflow: {e}")
-
 
